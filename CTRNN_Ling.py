3106693c3abeeec747b08a7b0f7a0e992e20d702
Ling Sun <91287479+LingSyrina@users.noreply.github.com>
2023-10-04 12:18:14 -0400

    Add files via upload

---
+++ CTRNN_Ling.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[6]:
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+# In[3]:
+
+
+class CTRNN():
+    '''CTRNN has a number of SENSOR, MOTOR, INTER neurons, 
+    each neuron has parameters (weights, time constants, biases, inputs),
+    (1) initial states are defined in init;
+    (2) states are updated in update;
+    (3) parameters are passed from GA to update'''
+    
+    def __init__(self,size,Connections):
+        
+        # morphology of this CTRNN
+        self.size = size
+        self.connections = Connections # the neurons connected to neuron_i: 1 for connected, 0 for not 
+        
+        # Parameters of this CTRNN: default fully connected
+        self.W = np.random.random(size=(self.size**2))
+        self.T = np.random.random(size=(self.size))
+        self.B = np.random.random(size=(self.size))
+        self.I = np.random.random(size=(self.size))
+        
+        # Initial State of CTRNN
+        self.inputs = np.zeros(self.size)
+        self.outputs = np.zeros(self.size)
+    
+    def receive_parameters(self,Parameters,Wrange,Trange,Brange,Irange): # receive weights from GA (set range)       
+        W = self.size**2
+        self.W = np.dot(Parameters[0:W],Wrange) 
+        self.T = abs(np.dot(Parameters[W:W+self.size],Trange[1]))+Trange[0]
+        self.B = np.dot(Parameters[W+self.size:W+self.size*2],Brange) 
+        self.I = np.dot(Parameters[W+self.size*2:W+self.size*3],Irange)   
+    
+    def get_outputs(self): # calculate the initial outputs        
+        for i in range(self.size):
+            self.outputs[i] = sigmoid(self.inputs[i] + self.B[i])
+            
+    def get_weights(self,index): # set weights for each neuron        
+        total_con = len(self.connections[0])       
+        W = self.W[index*total_con:index*total_con + total_con]       
+        return W
+       
+    def get_state(self,index,dt): # calculate the states for each neuron for each dt
+        W = self.get_weights(index)  
+        weighed_inputs = 0
+        for i in range(self.size):
+            weighed_inputs += W[i] * self.outputs[i] * self.connections[index][i]        
+        delta = (1/self.T[index]) * (-self.inputs[index] + weighed_inputs + self.I[index])
+        self.inputs[index] +=  delta * dt
+    
+    def get_state_ext (self,index,dt,external_input): # calculate the states for each neuron for each dt
+        W = self.get_weights(index)  
+        weighed_inputs = 0
+        for i in range(self.size):
+            weighed_inputs += W[i] * self.outputs[i] * self.connections[index][i]        
+        delta = (1/self.T[index]) * (-self.inputs[index] + weighed_inputs + self.I[index]* external_input[index])
+        self.inputs[index] +=  delta * dt
+    
+    def update (self,dt): # update all neurons per dt  
+        for i in range(self.size):
+            # update its current state
+            self.get_state(i,dt)
+            #  update its output
+            self.outputs[i] = sigmoid(self.inputs[i] + self.B[i])
+            
+    def update_ext (self,dt,external_input): # update all neurons per dt  
+        for i in range(self.size):
+            # update its current state
+            self.get_state_ext(i,dt,external_input)
+            #  update its output
+            self.outputs[i] = sigmoid(self.inputs[i] + self.B[i])
+
+
+# In[114]:
+
+
+# test CTRNN
+
+dt = 0.1
+time = np.arange(0,50,dt)
+o1 = np.zeros((len(time)))
+o2 = np.zeros((len(time)))
+i = 0
+
+a = CTRNN(2,[[1,1],[1,1]])
+a.receive_parameters([4.5, 1, -1, 4.5,1,1,-2.75,-1.75,0,0],1,[1,0],1,1)
+for t in time:
+    a.update(dt)
+    o1[i] = a.outputs[0]
+    o2[i] = a.outputs[1]
+    i +=1
+    
+plt.plot(time,o1)
+plt.plot(time,o2)
+
---
+++ GA.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[1]:
+
+
+class GA():
+    def __init__(self,pop,genesize):
+        self.pop = pop
+        self.gene = np.random.random((self.pop,genesize))
+        self.size = genesize 
+   
+    def MBG (self,perf,bestp,best,REC,MUT): # microbial search: compare two elements, mutate the loser
+        
+        # find two indexes
+        dem = self.pop/2
+        a = int(self.pop * np.random.uniform(0,1))
+        b = (a + 1 + int(dem * np.random.uniform(0,1))) % self.pop
+        
+        # compare
+        perf1 = perf(self.gene[a])
+        perf2 = perf(self.gene[b])
+        if perf1 > perf2: 
+            w=a
+            l=b
+            p = perf1
+        else:
+            w=b
+            l=a
+            p = perf2
+        
+        # print the best agent so far
+        if p > bestp:
+            bestp = p
+            best = self.gene[w]
+            besty = best.tolist() 
+            print('The best agent',bestp,besty)
+
+        # crossover and mutate
+        for j in range (self.size):
+            if REC > np.random.uniform(0,1): 
+                self.gene[l,j]= self.gene[w,j]
+            if MUT > np.random.uniform(0,1): 
+                self.gene[l,j] += np.random.normal(0,1)
+        for i in range(self.size):
+            self.gene[:,i] = np.clip(self.gene[:,i],-1,1)
+        return self, bestp, best
+
+    def run (self,perf,grt,REC,MUT): # Simulation: initialisation, each generation do MBG n times   
+        best_p = np.zeros(grt)
+        best_w = np.zeros((grt,self.size))
+        bestp = 0
+        best = np.zeros(self.size)
+        for n in range(grt): 
+            for i in range(int(pop)): 
+                gp, bestp, best = self.MBG(perf,bestp,best,REC,MUT)
+            best_p[n] = bestp
+            best_w[n] = best
+        x = np.arange(0,grt,1)
+        plt.plot(x,best_p)
+        plt.show()
+        return best_p,best_w
+
+
+# In[3]:
+
+
+# Test GA
+def fitness(genotype):
+    return np.mean(genotype)
+
+# parameters    
+perf = fitness
+pop = 10
+genesize = 2
+grt = 6
+REC = 0.3
+MUT = 0.1
+
+run = GA(pop,genesize)
+run.run(perf,grt,REC,MUT)  
+
