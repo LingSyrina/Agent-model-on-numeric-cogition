b48cbc9f3c6012cb4e3ab60e381237d1f662377b
Ling Sun <91287479+LingSyrina@users.noreply.github.com>
2023-10-05 01:42:40 -0400

    Update designed_agents.py

--- designed_agents.py
+++ designed_agents.py
@@ -2,74 +2,81 @@
 import math
 import matplotlib.pyplot as plt
 
-# Global parameters
-test = 3
-chase_sp = 150
-ini_agent, ini_circle, r, speed, number, distance = (3,0),(0,10),1,5, 3, 5
-# agent(ini_agent, ini_circle, r, speed, number, distance)
+class Circle:
+    def __init__(self, x, y, radius, speed):
+        self.x, self.y, self.radius, self.speed = x, y, radius, speed
+        
+    def fall(self, dt):
+        self.y = self.y - dt * self.speed if self.y - dt * self.speed >= 0 else -100
 
-def sense(pos_agent, pos_circle, r):
-    
-    # Agent's position & Circle's position
-    agent_x, agent_y = pos_agent
-    circle_x, circle_y = pos_circle
-    
-    # Sensor's field of view angle in degrees
-    sensor_fov = 60 * (math.pi / 180)
-    
-    # Calculate the horizental distance d
-    d = abs(agent_x - circle_x)
-    c = math.atan(sensor_fov) *  d
-    
-    # Check if the circle is within the sensor's range
-    if c - r/math.cos(sensor_fov) <= circle_y <= c + r/math.cos(sensor_fov):
-        deviation = (circle_y - c)/(r/math.cos(sensor_fov)) * math.pi/2
-        activity = math.cos(deviation) * 2 * r  # approximation of the activation (smoothed function based on the overlap size)
-    else:
-        activity = 0 
-    return (activity)
+class Agent:
+    def __init__(self, x, y):
+        self.x, self.y, self.memory, self.inertia = x, y, 0, 0
 
-def fall(speed, dt, ini_x, ini_y): 
-    return (ini_x, ini_y - dt * speed) if ini_y - dt * speed >= 0 else (-100,-100)
+    def sense(self, circle):
+        d, r, fov = abs(self.x - circle.x), circle.radius, math.radians(60)
+        c = math.atan(fov) * d
+        if c - r / math.cos(fov) <= circle.y <= c + r / math.cos(fov):
+            deviation = (circle.y - c) / (r / math.cos(fov)) * math.pi / 2
+            return math.cos(deviation) * 2 * r
+        return 0
+            
+    def recall(self, activity):
+        self.memory -= 1 if activity > 0 else 0
+    
+    def time(self, activity): 
+        self.inertia = 0 if activity > 0 else (self.inertia + 0.1 if activity == 0 else self.inertia)
+        
+    def update_memory(self, activity):
+        self.memory += 1 if activity > 0 else 0
 
-# habituation agent: inertia and activation required (two memory system)
-def agent(ini_agent, ini_circle, r, speed, number, distance):
-    
-    ini_x, ini_y = ini_circle
-    agent_x, agent_y = ini_agent
-    
-    # this is the sensing stage
-    memory = 0
-    for i in range(200):
-        dt = i/50
-        activity = 0
-        for n in range(number):
-            pos_circle = fall(speed, dt, ini_x, ini_y + n * distance)
-            activity += sense(ini_agent, pos_circle, r)
-        memory += (1 if activity > 0 else 0)
-    
-    # this is the catching stage: catch it if the number is the same
+    def update_position(self, chase_sp, dt):
+        if self.memory == 0 and self.inertia >= 1 and 0 < self.x <= 3:
+            self.x -= chase_sp * dt
+        if self.memory < 0 and 0 <= self.x < 3:
+            self.x += chase_sp * dt
+        
+
+def dehabituation(agent, circles, test):
     x, y, z = [], [], []
-    inertia = 0
-    for i in range(250): 
-        dt = i/50
-        activity = 0
-        for n in range(test):
-            pos_circle = fall(speed, dt, ini_x, ini_y + n * distance)
-            activity += sense((agent_x, agent_y), pos_circle, r)
-        if activity > 0:
-            memory -= 1
-            inertia = 0
-        else: 
-            inertia += 0.1
-        if memory == 0 and inertia >= 1 and agent_x > 0 : 
-            agent_x -= chase_sp * 1/50
-        if memory < 0 and inertia < 1 and agent_x < 3: 
-            agent_x += chase_sp * 1/50 
-        x.append(dt)
-        y.append(activity)
-        z.append(agent_x)       
     
+    for i in range(200):
+        dt = 1 / 50
+        activity = 0
+        for circle in circles:
+            circle.fall(dt)
+            activity += agent.sense(circle)
+        agent.update_memory(activity)
+    
+    circles = [Circle(*pos, r, speed) for pos in ini_circles] # modify
+    for i in range(250):
+        dt = 1 / 50
+        activity = 0
+        for circle in circles:
+            circle.fall(dt)
+            activity += agent.sense(circle)
+        agent.time(activity)
+        agent.recall(activity)
+        agent.update_position(chase_sp, 1 / 50)
+        x.append(i)
+        y.append(activity)
+        z.append(agent.x)
+
     plt.plot(x, y)
-    plt.plot(x,z)
+    plt.plot(x, z)
+
+# Global parameters
+test, chase_sp, ini_agent = 3, 150, (3, 0)
+ini_circles = [(0, 10), (0, 15), (0, 20)]
+r, speed, distance = 1, 5, 5
+
+# Create agent and circles
+agent = Agent(*ini_agent)
+circles = [Circle(*pos, r, speed) for pos in ini_circles]
+
+# Call agent_behavior with agent and circles
+dehabituation(agent, circles, test)
+
+# Display the plot
+plt.show()
     
